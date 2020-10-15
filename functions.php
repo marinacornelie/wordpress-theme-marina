fbf519441cbbc4fa562262aba041f77d5f3b56b4
Lolotte Schimmel <lmcschimmel@gmail.com>
2020-10-15 11:36:05 +0200

    add javascript and code cleanup

--- functions.php
+++ functions.php
@@ -1,22 +1,26 @@
 <?php 
 
 function register_my_menu() {
-  register_nav_menu('header-menu',__( 'Header Menu' ));
+	register_nav_menu('header-menu',__( 'Header Menu' ));
 }
-add_action( 'init', 'register_my_menu' );
 
 function my_deregister_scripts(){
-  wp_dequeue_script( 'wp-embed' );
- }
- 
- add_action( 'wp_footer', 'my_deregister_scripts' );
+	wp_dequeue_script( 'wp-embed' );
+}
 
- add_theme_support( 'custom-logo', array(
+function theme_scripts_function() {
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js');
+}
+
+add_theme_support( 'custom-logo', array(
 	'height'      => 100,
 	'width'       => 400,
 	'flex-height' => true,
 	'flex-width'  => true,
 	'header-text' => array( 'site-title', 'site-description' ),
-) );
+	) );
+add_action( 'init', 'register_my_menu' );
+add_action( 'wp_footer', 'my_deregister_scripts' );
+add_action('wp_enqueue_scripts','theme_scripts_function');
 
 ?>
