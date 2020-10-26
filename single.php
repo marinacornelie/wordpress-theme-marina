d89305decf0da91cbfe5ebeaa4678c89d5180995
Lolotte Schimmel <lmcschimmel@gmail.com>
2020-10-26 13:33:11 +0100

    add post header

--- single.php
+++ single.php
@@ -1,7 +1,17 @@
 <?php get_header(); ?>
-
+<?php the_post(); ?>
 <article class="hero">
     <main>
+    <header class="post-header post-header-border">
+            <div class="title is-6 fw-normal">
+                <span>By <?php the_author(); ?></span>
+            </div>
+            <div class="subtitle is-6">
+                <time><?php the_time('j M Y') ?></time>
+                <span>on <?php the_tags('', $sep, $after); ?></span>
+            <div>
+            <div class="title is-4 center"><?php the_title(); ?></div>
+        </header>
         <section class="section wp-content">
             <?php the_content(); ?>
         <section>
