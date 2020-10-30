69f6c4d67c6e85feafee3d9b0c5042909fb701ad
Lolotte Schimmel <lmcschimmel@gmail.com>
2020-10-30 14:13:28 +0100

    page template

--- page.php
+++ page.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
-
+<article class="hero">
+<header class="post-header post-header-border">
+        <div class="title is-4 center">
+            <?php echo get_the_title($ID); ?>
+        </div>
+    </header>
+    <section class="section wp-content">
+        <?= get_post_field('post_content', $post->ID); ?>
+    </section>
+</article>
 <?php get_footer(); ?>
